01dbf5020b120cf2deece27a8395cfd4c73a2a19
PogodinIERemote <igor.pogodin@vseinstrumenti.ru>
2023-06-04 23:24:01 +0300

    HW1

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	helloString := stringutil.Reverse("Hello, OTUS!")
+	fmt.Println(helloString)
 }
