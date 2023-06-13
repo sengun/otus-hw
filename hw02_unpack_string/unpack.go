035e030da6cd53248ad10e3c1cefdd7888aff610
PogodinIERemote <igor.pogodin@vseinstrumenti.ru>
2023-06-14 01:26:41 +0300

    linter fixes

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,29 +12,34 @@ const escapeRune = '\\'
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(input string) (string, error) {
-	var runes = []rune(input)
-	var output = strings.Builder{}
+	runes := []rune(input)
+	output := strings.Builder{}
 
 	for i := 0; i < len(runes); {
-		if i+2 < len(runes) && isSymbolEscapedWithCount(runes[i], runes[i+2]) {
-			var count, _ = strconv.Atoi(string(runes[i+2]))
+		switch {
+		case i+2 < len(runes) && isSymbolEscapedWithCount(runes[i], runes[i+2]):
+			count, _ := strconv.Atoi(string(runes[i+2]))
 			output.WriteString(strings.Repeat(string(runes[i+1]), count))
 
 			i += 3
-		} else if i+1 < len(runes) && isSymbolEscapedWithoutCount(runes[i]) {
+
+		case i+1 < len(runes) && isSymbolEscapedWithoutCount(runes[i]):
 			output.WriteString(strings.Repeat(string(runes[i+1]), 1))
 
 			i += 2
-		} else if i+1 < len(runes) && isSymbolNotEscapedWithCount(runes[i], runes[i+1]) {
-			var count, _ = strconv.Atoi(string(runes[i+1]))
+
+		case i+1 < len(runes) && isSymbolNotEscapedWithCount(runes[i], runes[i+1]):
+			count, _ := strconv.Atoi(string(runes[i+1]))
 			output.WriteString(strings.Repeat(string(runes[i]), count))
 
 			i += 2
-		} else if !unicode.IsDigit(runes[i]) && runes[i] != escapeRune {
+
+		case !unicode.IsDigit(runes[i]) && runes[i] != escapeRune:
 			output.WriteString(string(runes[i]))
 
 			i++
-		} else {
+
+		default:
 			return "", ErrInvalidString
 		}
 	}
@@ -55,11 +60,7 @@ func isSymbolEscapedWithCount(rune1 rune, rune3 rune) bool {
 }
 
 func isSymbolEscapedWithoutCount(rune1 rune) bool {
-	if rune1 != escapeRune {
-		return false
-	}
-
-	return true
+	return rune1 == escapeRune
 }
 
 func isSymbolNotEscapedWithCount(rune1 rune, rune2 rune) bool {
