02a83c06eb9d889ef2e15e7616ab0e80e896ef82
PogodinIERemote <igor.pogodin@vseinstrumenti.ru>
2023-09-05 01:12:14 +0300

    HW03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,75 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var dashExpression = regexp.MustCompile(`(\s-)|(-\s)`)
+var wordAppendixExpression = regexp.MustCompile(`[\n\t,.:;"']+`)
+
+type wordCountStruct struct {
+	word  string
+	count int
+}
+
+func Top10(text string) []string {
+	counter := make(map[string]int)
+
+	text = PrepareText(text)
+	for _, word := range GetTextUnits(text) {
+		if len(word) == 0 {
+			continue
+		}
+		lowerCaseWord := strings.ToLower(word)
+		counter[lowerCaseWord] = counter[lowerCaseWord] + 1
+	}
+	wordCounter := make([]wordCountStruct, 0, len(counter))
+
+	for word, wordCount := range counter {
+		wordCounter = append(wordCounter, wordCountStruct{word, wordCount})
+	}
+
+	return GetFirstTenWords(SortWordCountStruct(wordCounter))
+}
+
+func PrepareText(text string) string {
+	textWithoutDashes := dashExpression.ReplaceAll([]byte(text), []byte(" "))
+	textWithoutAppendix := wordAppendixExpression.ReplaceAll(textWithoutDashes, []byte(" "))
+
+	return string(textWithoutAppendix)
+}
+
+func GetTextUnits(text string) []string {
+	if text == "" {
+		return []string{}
+	}
+
+	return strings.Split(text, " ")
+}
+
+func GetFirstTenWords(wordCounter []wordCountStruct) []string {
+	j := 0
+	resultWords := make([]string, 0, len(wordCounter))
+	for _, wordCount := range wordCounter {
+		resultWords = append(resultWords, wordCount.word)
+		j++
+		if j >= 10 {
+			break
+		}
+	}
+
+	return resultWords
+}
+
+func SortWordCountStruct(wordCounter []wordCountStruct) []wordCountStruct {
+	sort.Slice(wordCounter, func(i, j int) bool {
+		if wordCounter[i].count == wordCounter[j].count {
+			return wordCounter[i].word < wordCounter[j].word
+		}
+		return wordCounter[i].count > wordCounter[j].count
+	})
+
+	return wordCounter
 }
