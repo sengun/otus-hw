d217cacdd5b7ebd1903fe77c9e6968bda51be943
PogodinIERemote <igor.pogodin@vseinstrumenti.ru>
2023-09-13 23:45:41 +0300

    HW04 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,15 +11,71 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	mutex    *sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+
+	if listItem, exists := cache.items[key]; exists {
+		cacheItem := listItem.Value.(*cacheItem)
+		cacheItem.value = value
+		cache.queue.MoveToFront(listItem)
+
+		return true
+	}
+	listItem := cache.queue.PushFront(&cacheItem{key: key, value: value})
+	cache.items[key] = listItem
+	if cache.queue.Len() > cache.capacity {
+		cache.DisplaceLastItem()
+	}
+
+	return false
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+
+	if listItem, exists := cache.items[key]; exists {
+		cache.queue.MoveToFront(listItem)
+		cacheItem := listItem.Value.(*cacheItem)
+		return cacheItem.value, true
+	}
+
+	return nil, false
+}
+
+func (cache *lruCache) Clear() {
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+
+	cache.queue = NewList()
+	cache.items = map[Key]*ListItem{}
+}
+
+func (cache *lruCache) DisplaceLastItem() {
+	lastListItem := cache.queue.Back()
+	if lastListItem == nil {
+		return
+	}
+	cacheItem := lastListItem.Value.(*cacheItem)
+	delete(cache.items, cacheItem.key)
+	cache.queue.Remove(lastListItem)
+}
+
+type cacheItem struct {
+	key   Key
+	value interface{}
+}
+
 func NewCache(capacity int) Cache {
 	return &lruCache{
+		mutex:    &sync.Mutex{},
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,86 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	itemsCount int
+	FrontItem  *ListItem
+	BackItem   *ListItem
+}
+
+func (l *list) Len() int {
+	return l.itemsCount
+}
+
+func (l *list) Front() *ListItem {
+	return l.FrontItem
+}
+
+func (l *list) Back() *ListItem {
+	return l.BackItem
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newListItem := &ListItem{
+		Value: v,
+		Next:  l.FrontItem,
+		Prev:  nil,
+	}
+	if l.FrontItem != nil {
+		l.FrontItem.Prev = newListItem
+	}
+	if l.BackItem == nil {
+		l.BackItem = newListItem
+	}
+	l.FrontItem = newListItem
+	l.itemsCount++
+
+	return newListItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newListItem := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  l.BackItem,
+	}
+	if l.BackItem != nil {
+		l.BackItem.Next = newListItem
+	}
+	if l.FrontItem == nil {
+		l.FrontItem = newListItem
+	}
+	l.BackItem = newListItem
+	l.itemsCount++
+
+	return newListItem
+}
+
+func (l *list) Remove(item *ListItem) {
+	if item == nil {
+		return
+	}
+
+	if item.Prev != nil {
+		item.Prev.Next = item.Next
+	} else {
+		l.FrontItem = item.Next
+	}
+
+	if item.Next != nil {
+		item.Next.Prev = item.Prev
+	} else {
+		l.BackItem = item.Prev
+	}
+
+	l.itemsCount--
+}
+
+func (l *list) MoveToFront(item *ListItem) {
+	if l.FrontItem == item {
+		return
+	}
+
+	l.Remove(item)
+	l.PushFront(item.Value)
 }
 
 func NewList() List {
